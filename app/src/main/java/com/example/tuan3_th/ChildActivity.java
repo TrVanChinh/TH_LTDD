0ff73b005e2c052e3314398327911feb8c4b6f34
TrVanChinh <chinhtran0964758845@gmail.com>
2022-09-08 19:41:26 +0700

    tao Activity va di chuyen giua cac activity

---
+++ app/src/main/java/com/example/tuan3_th/ChildActivity.java
@@ -0,0 +1,26 @@
+package com.example.tuan3_th;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChildActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+        Button btnBack= (Button)
+                findViewById(R.id.btnBacktoMainActivity);
+        btnBack.setOnClickListener(new
+                                           View.OnClickListener() {
+                                               public void onClick(View v) {
+                                                   finish();
+                                               }
+                                           });
+    }
+}
---
+++ app/src/main/java/com/example/tuan3_th/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.tuan3_th;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnOpen=(Button) findViewById(R.id.btnOpen);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+                                               public void onClick(View arg0) {
+                                                   doOpenChildActivity();
+                                               }
+                                           });
+    }
+    public void doOpenChildActivity()
+    {
+        Intent myIntent=new Intent(this, ChildActivity.class);
+        startActivity(myIntent);
+    }
+}
